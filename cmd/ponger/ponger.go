28a8ec3af426b2759152ce8c4e5056fb39f5d96b
thinkofher <mail@bdudek.xyz>
2021-06-26 15:36:41 +0200

    Add API key authorization.

--- cmd/ponger/ponger.go
+++ cmd/ponger/ponger.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/alexandrevicenzi/go-sse"
 
@@ -10,16 +11,40 @@ import (
 	"github.com/hauru-club/ping/pkg/handlers"
 )
 
+const (
+	secretKeyEnv          = "PONGER_SECRET"
+	defaultSecretKeyValue = "supersecretkey"
+
+	portEnv          = "PONGER_PORT"
+	defaultPortValue = "5000"
+
+	addressEnv          = "PONGER_ADDRESS"
+	defaultAddressValue = "0.0.0.0"
+)
+
+func readSetting(env, fallback string) string {
+	res := os.Getenv(env)
+	if res == "" {
+		return fallback
+	}
+	return res
+}
+
 func main() {
+	secretKey := readSetting(secretKeyEnv, defaultSecretKeyValue)
+	port := readSetting(portEnv, defaultPortValue)
+	address := readSetting(addressEnv, defaultAddressValue)
+
 	s := sse.NewServer(nil)
 	defer s.Shutdown()
 
 	http.Handle("/", handlers.Bytes(assets.Index))
 	http.Handle("/events/", s)
-	http.Handle("/publish", handlers.Publish(s))
+
+	http.Handle("/publish", handlers.AuthKeyMiddleware(secretKey)(handlers.Publish(s)))
 	http.Handle("/assets/", http.StripPrefix("/assets/",
 		http.FileServer(http.FS(assets.StaticFiles))),
 	)
-	log.Println("Listening at :5000")
-	http.ListenAndServe("0.0.0.0:5000", nil)
+	log.Println("Listening at", address+":"+port)
+	http.ListenAndServe(address+":"+port, nil)
 }
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -25,6 +25,26 @@ func internalServerError(w http.ResponseWriter) {
 
 }
 
+func unauthorized(w http.ResponseWriter) {
+	http.Error(w, "Given publish key is invalid", http.StatusUnauthorized)
+}
+
+const authHeaderKey = "Auth-Publish-Key"
+
+// AuthKeyMiddleware verifies if request header "Auth-Publish-Key" contains
+// given key. If not, terminates connection with http status Unauthorized.
+func AuthKeyMiddleware(key string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if inKey := r.Header.Get(authHeaderKey); inKey != key {
+				unauthorized(w)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
 // Publish is handler for publishing ICMP packets to
 // SSE clients.
 func Publish(s *sse.Server) http.HandlerFunc {
